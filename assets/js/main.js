31db101c350e57906b265863566d99eca35a2180
Truong Pham <phamcse@berkeley.edu>
2016-06-30 08:58:43 -0700

    major update to mywebsite

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,4 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function ($) {
-
 
 	var settings = {
 
@@ -14,9 +7,7 @@
 
 		// Parallax factor (lower = more intense, higher = less intense).
 		parallaxFactor: 20
-
 	};
-
 
 	skel
 		.breakpoints({
@@ -93,49 +84,10 @@
 					$window.on('scroll.strata_parallax', function () {
 						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 					});
-
 				}
-
 			});
-
 		}
 
-		// Main Sections: Two.
-
-		// Lightbox gallery.
-		$window.on('load', function () {
-
-			$('#two').poptrox({
-				caption: function ($a) {
-					return $a.next('h3').text();
-				},
-				overlayColor: '#2c2c2c',
-				overlayOpacity: 0.85,
-				popupCloserText: '',
-				popupLoaderText: '',
-				selector: '.work-item a.image',
-				usePopupCaption: true,
-				usePopupDefaultStyling: false,
-				usePopupEasyClose: false,
-				usePopupNav: true,
-				windowMargin: (skel.breakpoint('small').active ? 0 : 50)
-			});
-
-		});
-
-
-		/////////////
-
-
-
-
-
-		// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500,
-				offset: $header.outerHeight()
-			});
 
 		// Menu.
 		$('#menu')
@@ -176,121 +128,129 @@
 			});
 
 		}
-
 	});
 
 })(jQuery);
 
 particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 200,
-      "density": {
-        "enable": !0,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#f0f0f0"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.3,
-      "random": !1,
-      "anim": {
-        "enable": !1,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": !1
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": !0,
-      "anim": {
-        "enable": !1,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": !1
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 110,
-      "color": "#ffffff",
-      "opacity": 0.15,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 2,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 130,
-        "line_linked": {
-          "opacity": .3
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
+	"particles": {
+		"number": {
+			"value": 200,
+			"density": {
+				"enable": !0,
+				"value_area": 800
+			}
+		},
+		"color": {
+			"value": "#d9d9d9"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 0,
+				"color": "#0000ff"
+			},
+			"polygon": {
+				"nb_sides": 5
+			},
+			"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+			}
+		},
+		"opacity": {
+			"value": 0.3,
+			"random": !1,
+			"anim": {
+				"enable": !1,
+				"speed": 1,
+				"opacity_min": 0.1,
+				"sync": !1
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": !0,
+			"anim": {
+				"enable": !1,
+				"speed": 40,
+				"size_min": 0.1,
+				"sync": !1
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 110,
+			"color": "#d9d9d9",
+			"opacity": 0.15,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 2,
+			"direction": "none",
+			"random": true,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+				"enable": false,
+				"rotateX": 600,
+				"rotateY": 1200
+			}
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "grab"
+			},
+			"onclick": {
+				"enable": true,
+				"mode": "push"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+				"distance": 130,
+				"line_linked": {
+					"opacity": .3
+				}
+			},
+			"bubble": {
+				"distance": 400,
+				"size": 40,
+				"duration": 2,
+				"opacity": 8,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 200,
+				"duration": 0.4
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+		}
+	},
+	"retina_detect": true
 });
+
+
+$(function () {
+	$('#two').poptrox({
+    popupPadding: 0
+	});
+});
+
+	
 
 
 var count_particles, stats, update;
@@ -301,15 +261,12 @@ stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
 count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
+update = function () {
+	stats.begin();
+	stats.end();
+	if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+		count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+	}
+	requestAnimationFrame(update);
 };
 requestAnimationFrame(update);;
-
-
-
