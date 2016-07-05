75e3af26d95b512584421b49c11ad37fa34524ed
Truong Pham <phamcse@berkeley.edu>
2016-07-04 18:07:08 -0700

    change display for count-particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,7 +260,6 @@ var count_particles, stats, update;
 stats = new Stats;
 stats.setMode(0);
 stats.domElement.style.position = 'absolute';
-stats.domElement.style.z-index = '100';
 stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
